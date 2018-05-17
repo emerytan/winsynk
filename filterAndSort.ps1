6fccff63263e53534baca62c6e336dcae918bfdd
emerytan <emery.t.anderson@gmail.com>
2018-05-17 01:08:47 -0400

    poweshell script to seperate _shots dirs then run delete filters

---
+++ filterAndSort.ps1
@@ -0,0 +1,18 @@
+﻿$localPath = "Z:\VFX\_shots\*\*\*"
+$here = "C:\Users\localAdmin\nodeApps\tmp"
+$arr = Get-ChildItem Z:\VFX\_shots | Where-Object {$_.PSIsContainer} | ForEach-Object {$_.Name}
+$me = Get-ChildItem $localPath  | Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
+
+$output = foreach ($a in $me) {
+    Get-ChildItem $a.FullName |
+        Sort-Object -Descending |
+        Select-Object -skip 3 |
+        Select-Object -ExpandProperty FullName
+}
+
+for ($i = 0; $i -lt $arr.Length; $i++) {
+    $logfile = $(Join-Path $here $arr[$i]) + "_doNotSync.txt"
+    $output | Select-String -Pattern $arr[$i] |
+        Out-File -FilePath $logfile
+}
+
