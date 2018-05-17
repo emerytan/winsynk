89c822b665c9c98a0bcc2aea1389363b46936fd7
emerytan <emery.t.anderson@gmail.com>
2018-05-16 22:09:40 -0400

    poweshell filters added

---
+++ filter1.ps1
@@ -0,0 +1,10 @@
+﻿$localPath = "Z:\VFX\_shots\*\*\*"
+$here = "C:\Users\localAdmin\nodeApps\tmp"
+$me = Get-ChildItem $localPath  | Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
+$output = foreach ($a in $me) { dir $a.FullName | Sort-Object -Descending | Select-Object -skip 3 | Select-Object -ExpandProperty FullName }
+$output | Select-Object | Out-File -FilePath "$here/outFile.txt" -Encoding ascii
+
+
+#$xlist = "`"$((Get-Content C:\Users\Admin\Desktop\test2.txt) -join '"`n"')`""
+#$xlist | Out-File -FilePath "C:\Users\Admin\Desktop\test5.txt" -Encoding ascii
+
---
+++ filter2.ps1
@@ -0,0 +1,24 @@
+﻿# ETA's shots delete filter powershell script
+
+$localPath = "Z:\VFX\_shots\*\*\*"
+$here = "C:\Users\localAdmin\nodeApps\tmp"
+
+Get-ChildItem $localPath  |
+    Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
+
+
+$output = foreach ($a in $me) {
+    dir $a.FullName |
+    Sort-Object -Descending |
+    Select-Object -skip 3 |
+    Select-Object -ExpandProperty FullName
+#    Out-File -FilePath "$here/outFile.txt" -Encoding ascii    
+}
+
+
+
+# shots robocopy command
+# robocopy Y:\VFX\_shots\ Z:\VFX\_shots\ /xd (Get-Content .\nodeApps\fixlist.txt | Select-Object -First 650) /xf "Thumbs.db" ".DS_Store" /mir /r:0 /w:0 /mon:100 /mot:30
+
+# assets robocopy command
+# robocopy \\10.34.140.250\panama\VFX\_assets\ Z:\VFX\_assets\ /xf "Thumbs.db", ".DS_Store" /xd "mdl_*", "setref_*" /mir /r:0 /w:0 /mon:100 /mot:30
