176ac26e981bc42375674048a14f9e2384e41b48
emerytan <emery.t.anderson@gmail.com>
2018-05-22 11:46:36 -0400

    filter and sync improved directory list propegation but robocopy loop still doesn't work.  Probably needs to be a function x

--- filterAndSort.ps1
+++ filterAndSort.ps1
@@ -1,17 +1,17 @@
-﻿# $localPath = "Y:\VFX\_shots\*\*\*"
-# $here = "C:\Users\localAdmin\nodeApps\tmp\"
-# $arr = Get-ChildItem Z:\VFX\_shots | Where-Object {$_.PSIsContainer} | ForEach-Object {$_.Name}
-# $me = Get-ChildItem $localPath  | Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
+﻿$localPath = "Y:\VFX\_shots\*\*\*"
+$here = "C:\Users\localAdmin\nodeApps\tmp\"
+$arr = Get-ChildItem Z:\VFX\_shots | Where-Object {$_.PSIsContainer} | ForEach-Object {$_.Name}
+$me = Get-ChildItem $localPath  | Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
 
 $allDirs = "C:\Users\localAdmin\nodeApps\tmp\allDirs.txt"
 
-# foreach ($a in $me) {
-#     Get-ChildItem $a.FullName |
-#         Sort-Object -Descending |
-#         Select-Object -skip 3 |
-#         Select-Object -ExpandProperty FullName |
-#         Out-File -FilePath $($here + "scriptAll.txt") -Append
-# }
+foreach ($a in $me) {
+    Get-ChildItem $a.FullName |
+        Sort-Object -Descending |
+        Select-Object -skip 3 |
+        Select-Object -ExpandProperty FullName |
+        Out-File -FilePath $($here + "scriptAll.txt") -Append
+}
 
 
 for ($i = 0; $i -lt $arr.Length; $i++) {
