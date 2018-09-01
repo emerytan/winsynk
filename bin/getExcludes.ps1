6cc7958c77a212a874418d8302cc72d87f447c90
emerytan <emery.t.anderson@gmail.com>
2018-09-01 02:38:08 -0400

    robocopy loop first run

--- bin/getExcludes.ps1
+++ bin/getExcludes.ps1
@@ -1,6 +1,11 @@
-﻿$localPath = "Z:\VFX\_shots\*\*\*"
-$here = "C:\Users\NUTCRACKER\nodeApps\winsynk\tmp"
-$arr = Get-ChildItem Z:\VFX\_shots | Where-Object {$_.PSIsContainer} | ForEach-Object {$_.Name}
+﻿$localPath = "Y:\VFX\_shots\*\*\*"
+$here = "C:\Users\localAdmin\nodeApps\synkerRelease\tmp"
+$arr = Get-ChildItem "Y:\VFX\_shots" | Where-Object {$_.PSIsContainer} | ForEach-Object {$_.Name}
+Write-Host "Drilling into the following directories"
+Write-Host $arr
+
+Start-Sleep -Seconds 3
+Write-Host "Working..."
 $me = Get-ChildItem $localPath  | Where-Object {$_.Name -match "anim_|comp_|lyt_|ref_Main|pcomp" -and $_.PsIsContainer}
 
 
@@ -14,7 +19,9 @@ $output = foreach ($a in $me) {
 
 for ($i = 0; $i -lt $arr.Length; $i++) {
     $logfile = $(Join-Path $here $arr[$i]) + "_doNotSync.txt"
+    Write-Host "Outputting file: $($logfile)"
     $output | Select-String -Pattern $arr[$i] |
     Out-File -FilePath $logfile
+    Start-Sleep 3
 }
 
--- bin/roboLoop.ps1
+++ bin/roboLoop.ps1
@@ -3,11 +3,14 @@ $excludeListPath = "C:\Users\$env:USERNAME\nodeApps\synkerRelease\tmp"
 $plateExcludes = "C:\Users\$env:USERNAME\nodeApps\synkerRelease\tmp\plateExcludes.txt"
 
 for ($i = 0; $i -lt $shotsPaths.Length; $i++) {
+
     $base = Split-Path $shotsPaths[$i] -Leaf
-    $destination = Join-Path Z:\VFX\_shots $base
+    $destination = Join-Path "Z:\VFX\_shots" $base
+
+    Start-Sleep -Seconds 2
+    Robocopy.exe $shotsPaths[$i] $destination /e /maxage:30 /njh /np /xd 'ref_Main' (Get-ChildItem $excludeListPath | Where-Object {$_.Name -Match $(Split-Path $shotsPaths[$i] -Leaf)} | Get-Content) (Get-Content $plateExcludes) /xf '.DS_Store', 'Thumbs.db' /w:0 /r:0
     Write-Host "source: $($shotsPaths[$i]) -- dest: $($destination)"
-    Start-Sleep -Seconds 5
-    Robocopy.exe $shotsPaths[$i] $destination /l /nfl /ndl /njh /e /xd (Get-ChildItem $excludeListPath | Where-Object {$_.Name -Match $(Split-Path $shotsPaths[$i] -Leaf)} | Get-Content) (Get-Content $plateExcludes) /xf '.DS_Store', 'Thumbs.db' /w:0 /r:0
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 8
+
 }
 
